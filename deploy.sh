9742fe189da3ff0d2116d70da3f34b645b270fab
AmmarAliShah <syedammarali24@gmail.com>
2015-11-27 08:40:19 +0500

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+chmod +x deploy.sh
+
+cd Ammar-Says
+jekyll build --destination ../ammaralishah.github.io
+git add -- all
+git commit -m "Commit 01 | Auto"
+git push -u origin master
+cd ..
+cd ammaralishah.github.io
+git add --all
+git commit -m "Commit 02 | Auto"
+git push -u origin master
